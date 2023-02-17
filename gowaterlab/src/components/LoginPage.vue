bcc8803f66b852c8b6b4f6ee66b9696dcd4889ba
xuanjia <xuanjia961121@gmail.com>
2023-02-17 10:50:50 +0800

    ADD

--- gowaterlab/src/components/LoginPage.vue
+++ gowaterlab/src/components/LoginPage.vue
@@ -1,13 +1,16 @@
 <template lang="">
     <div>
-        <h5>123456</h5>
+        <h5>123456789</h5>
+        <h6><h6>
     </div>
 </template>
+
 <script>
 export default {
     
 }
 </script>
+
 <style lang="">
     
 </style>
